35e813b77e47d08df15e4dcf6b98a74780bb6e41
DePopadopalis <DePopadopalis@outlook.com>
2018-08-16 23:33:13 -0400

    Transition to using conemu + oh-my-posh for powershell colorscheme

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,9 +1,10 @@
 # Import Powershell modules
 Import-Module posh-git
+Import-Module oh-my-posh
 
 # Set Aliases
-New-Alias which Get-Command
-New-Alias touch New-Item
+# New-Alias which Get-Command   # Not needed with ConEmu
+# New-Alias touch New-Item      # Not needed with ConEmu
 
 # Disable beeping noise
 Set-PSReadlineOption -BellStyle None
@@ -12,6 +13,7 @@ Set-PSReadlineOption -BellStyle None
 # Available color options:
 #   White |   Blue   |   Green   |   Cyan   |   Red   |   Magenta   |   Yellow   | Gray
 #   Black | DarkBlue | DarkGreen | DarkCyan | DarkRed | DarkMagenta | DarkYellow | DarkGray
+<#
 Set-PSReadlineOption -TokenKind comment -ForegroundColor Black
 Set-PSReadlineOption -TokenKind none -ForegroundColor DarkBlue
 Set-PSReadlineOption -TokenKind command -ForegroundColor DarkGreen
@@ -22,3 +24,4 @@ Set-PSReadlineOption -TokenKind number -ForegroundColor DarkMagenta
 Set-PSReadlineOption -TokenKind string -ForegroundColor Black
 Set-PSReadlineOption -TokenKind operator -ForegroundColor DarkBlue
 Set-PSReadlineOption -TokenKind member -ForegroundColor DarkBlue
+#>
