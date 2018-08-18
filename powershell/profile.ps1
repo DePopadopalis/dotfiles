e0dc8ff32eb27f3f97e1746e843aab9d0acd184b
DePopadopalis <DePopadopalis@outlook.com>
2018-08-18 07:21:41 -0400

    Add comments for easier color translation

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -5,23 +5,52 @@ Import-Module oh-my-posh
 # Set Aliases
 # New-Alias which Get-Command   # Not needed with ConEmu
 # New-Alias touch New-Item      # Not needed with ConEmu
+# Set l and ls alias to use the new Get-ChildItemColor cmdlets
+Set-Alias ll Get-ChildItemColor -Option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
 
 # Disable beeping noise
 Set-PSReadlineOption -BellStyle None
 
 # Set Powershell colorscheme
 # Available color options:
-#   White |   Blue   |   Green   |   Cyan   |   Red   |   Magenta   |   Yellow   | Gray
-#   Black | DarkBlue | DarkGreen | DarkCyan | DarkRed | DarkMagenta | DarkYellow | DarkGray
-<#
-Set-PSReadlineOption -TokenKind comment -ForegroundColor Black
-Set-PSReadlineOption -TokenKind none -ForegroundColor DarkBlue
-Set-PSReadlineOption -TokenKind command -ForegroundColor DarkGreen
-Set-PSReadlineOption -TokenKind parameter -ForegroundColor DarkGray
+#   White=dark_teal |   Blue=deepskyblue   |   Green=seagreen   |
+#   Cyan=ice_blue   |   Red=orange  |   Magenta=bright_green   |
+#   Yellow=gold   | Gray=dimgray
+#   Black=snow | DarkBlue=blue | DarkGreen=darkseagreen |
+#   DarkCyan=cerulean | DarkRed=crimson | DarkMagenta=purple |
+#   DarkYellow=goldenrod | DarkGray=black
+
+Set-PSReadlineOption -TokenKind comment -ForegroundColor DarkGray
+Set-PSReadlineOption -TokenKind none -ForegroundColor White
+Set-PSReadlineOption -TokenKind command -ForegroundColor DarkBlue
+Set-PSReadlineOption -TokenKind parameter -ForegroundColor Magenta
 Set-PSReadlineOption -TokenKind variable -ForegroundColor DarkRed
-Set-PSReadlineOption -TokenKind type -ForegroundColor DarkBlue
-Set-PSReadlineOption -TokenKind number -ForegroundColor DarkMagenta
-Set-PSReadlineOption -TokenKind string -ForegroundColor Black
-Set-PSReadlineOption -TokenKind operator -ForegroundColor DarkBlue
-Set-PSReadlineOption -TokenKind member -ForegroundColor DarkBlue
-#>
+Set-PSReadlineOption -TokenKind type -ForegroundColor DarkMagenta
+Set-PSReadlineOption -TokenKind number -ForegroundColor Red
+Set-PSReadlineOption -TokenKind string -ForegroundColor DarkCyan
+Set-PSReadlineOption -TokenKind operator -ForegroundColor White
+Set-PSReadlineOption -TokenKind member -ForegroundColor White
+
+Set-Theme DeepBlueGrove
+
+# FUNCTIONS
+function U
+{
+    param
+    (
+        [int] $Code
+    )
+ 
+    if ((0 -le $Code) -and ($Code -le 0xFFFF))
+    {
+        return [char] $Code
+    }
+ 
+    if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+    {
+        return [char]::ConvertFromUtf32($Code)
+    }
+ 
+    throw "Invalid character code $Code"
+}
